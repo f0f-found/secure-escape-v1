4c942ade4902a2551b5ce3adb0971d6e551d708e
Rorisang Putu <rorisangputu@gmail.com>
2026-06-10 18:40:52 +0200

    feat: Add comments to ProtectedRoute for better code clarity

--- SecureEscapeDash/src/App.tsx
+++ SecureEscapeDash/src/App.tsx
@@ -4,7 +4,10 @@ import Dashboard from "./pages/Dashboard";
 import { getToken } from "./utils/tokenStore";
 import "./App.css";
 function ProtectedRoute({ children }: { children: React.ReactNode }) {
+  //Fetching auth token to check if a user is logged in.
   const token = getToken();
+
+  //Checks if there's a token , if not route user to login
   return token ? <>{children}</> : <Navigate to="/login" replace />;
 }
 
