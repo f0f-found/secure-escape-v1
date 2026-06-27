e69ed3cd22fa6aeb36d906fa0a5551b9a48d1d6c
Rorisang Putu <rorisangputu@gmail.com>
2026-06-27 18:36:41 +0200

    feat: Add methods to INotificationAttemptRepository for retrieving notification attempts

--- SecureEscape.Api/Interfaces/INotificationAttemptRepository.cs
+++ SecureEscape.Api/Interfaces/INotificationAttemptRepository.cs
@@ -5,4 +5,6 @@ namespace SecureEscape.Api.Interfaces;
 public interface INotificationAttemptRepository
 {
     Task AddAsync(NotificationAttempt notificationAttempt);
+    Task<NotificationAttempt?> GetByIdAsync(Guid id);
+    Task<List<NotificationAttempt>> GetPendingAsync();
 }
