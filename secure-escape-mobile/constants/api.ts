b9fe1b7c1333a6bee78f49d3e28e3b1b334e1cdf
Rorisang Putu <rorisangputu@gmail.com>
2026-07-23 18:25:08 +0200

    Adding azure api connection link

--- secure-escape-mobile/constants/api.ts
+++ secure-escape-mobile/constants/api.ts
@@ -1,7 +1,8 @@
 import { Platform } from "react-native";
 
 //const LOCAL_IP = "192.168.1.103";
-const LOCAL_IP = "192.168.8.153";
+// const LOCAL_IP = "192.168.8.153";
 
-export const API_BASE_URL =
-  Platform.OS === "web" ? "http://localhost:5116" : `http://${LOCAL_IP}:5116`;
+// export const API_BASE_URL =
+//   Platform.OS === "web" ? "http://localhost:5116" : `http://${LOCAL_IP}:5116`;
+export const API_BASE_URL = process.env.EXPO_PUBLIC_API_BASE_URL!;
